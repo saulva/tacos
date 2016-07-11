1e820eb62fee26ba8d8e8d6583552452502e296b
saulva <vaso139277@upemor.edu.mx>
2016-07-10 19:16:51 -0500

    proyectos

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({full_width: true});
+    $('.tooltipped').tooltip({delay: 50});
+  });
+
+})(jQuery);
